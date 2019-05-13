5b7e90621bd1445b182ef82c6807d141ea616e7c
Daniel Ashraff <danashraff15@gmail.com>
2019-05-13 12:28:59 +0800

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php // define database connection variables as contant
+
+define('DB_USER', "root");//db user
+
+define('DB_PASSWORD', "");//db password	
+
+define('DB_DATABASE', "07_Daniel_pess");//database name
+
+define('DB_SERVER', "localhost");//db server
+
+?>
